fbb693d89e28f239de42da815df34374ea91928f
christine chemali <christine.chemali@laplateforme.io>
2025-04-23 08:47:07 +0200

    Add reset password entry to forgot password window

--- source/UiForgotPassword.c
+++ source/UiForgotPassword.c
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+#include "Utils.h"
 
 // had to change for future action connect database
 static void handle_forgot_password(GtkWidget *widget, gpointer data)
@@ -64,7 +65,51 @@ void show_forgot_password_content(GtkWidget *stack, GCallback return_to_login_ca
     gtk_box_append(GTK_BOX(question_container), authenticate_second_question_label);
     gtk_box_append(GTK_BOX(question_container), authenticate_second_question_entry);
 
-    
+    //Password
+    GtkWidget *reset_password_container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_widget_set_size_request(reset_password_container, container_width, -1);
+    gtk_widget_set_halign(reset_password_container, GTK_ALIGN_CENTER);
+
+    GtkWidget *new_password_label = gtk_label_new("Enter your new password:");
+    gtk_widget_set_halign(new_password_label, GTK_ALIGN_START);
+    gtk_widget_set_css_classes(new_password_label, (const char *[]){"custom-label", NULL}); // css
+
+    GtkWidget *reset_password_label = gtk_label_new("Password*:");
+    gtk_widget_set_halign(reset_password_label, GTK_ALIGN_START);
+    gtk_widget_set_css_classes(reset_password_label, (const char *[]){"custom-label", NULL}); // css
+
+    GtkWidget *reset_password_entry = gtk_entry_new(); 
+    gtk_entry_set_visibility(GTK_ENTRY(reset_password_entry), FALSE);
+    gtk_widget_set_size_request(reset_password_entry, 200, -1);
+    gtk_widget_set_css_classes(reset_password_entry, (const char *[]){"custom-entry", NULL}); // css
+
+    GtkWidget *password_label_checkbox_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+    gtk_box_set_spacing(GTK_BOX(password_label_checkbox_box), 115); 
+
+    GtkWidget *reset_retype_password_label = gtk_label_new("Re-type password*:");
+    gtk_widget_set_halign(reset_retype_password_label, GTK_ALIGN_START);
+    gtk_widget_set_css_classes(reset_retype_password_label, (const char *[]){"custom-label", NULL}); // css
+
+    GtkWidget *reset_retype_password_entry = gtk_entry_new();
+    gtk_entry_set_visibility(GTK_ENTRY(reset_retype_password_entry), FALSE);
+    gtk_widget_set_size_request(reset_retype_password_entry, 200, -1);
+    gtk_widget_set_css_classes(reset_retype_password_entry, (const char *[]){"custom-entry", NULL}); // css
+
+    GtkWidget *show_account_password_check = gtk_check_button_new_with_label("Show");
+    gtk_widget_set_halign(show_account_password_check, GTK_ALIGN_END);
+    g_signal_connect(show_account_password_check, "toggled", G_CALLBACK(toggle_passwordVisibility), reset_password_entry);
+    g_signal_connect(show_account_password_check, "toggled", G_CALLBACK(toggle_passwordVisibility), reset_retype_password_entry);
+    gtk_widget_set_css_classes(show_account_password_check, (const char *[]){"custom-checkbox", NULL}); // css
+
+    gtk_box_append(GTK_BOX(password_label_checkbox_box), reset_password_label);
+    gtk_box_append(GTK_BOX(password_label_checkbox_box), show_account_password_check);
+
+    gtk_box_append(GTK_BOX(reset_password_container), new_password_label);
+    gtk_box_append(GTK_BOX(reset_password_container), password_label_checkbox_box);
+    gtk_box_append(GTK_BOX(reset_password_container), reset_password_entry);
+    gtk_box_append(GTK_BOX(reset_password_container), reset_retype_password_label);
+    gtk_box_append(GTK_BOX(reset_password_container), reset_retype_password_entry);
+
     //Buttons : Change and return to login
     GtkWidget *change_buttons_container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     gtk_widget_set_size_request(change_buttons_container, container_width, -1);
@@ -86,6 +131,7 @@ void show_forgot_password_content(GtkWidget *stack, GCallback return_to_login_ca
     // Add container to main box
     gtk_box_append(GTK_BOX(forgot_box), change_password_title_container);
     gtk_box_append(GTK_BOX(forgot_box), question_container);
+    gtk_box_append(GTK_BOX(forgot_box), reset_password_container);
     gtk_box_append(GTK_BOX(forgot_box), change_buttons_container);
 
     gtk_stack_add_named(GTK_STACK(stack), forgot_box, "forgot_password");
