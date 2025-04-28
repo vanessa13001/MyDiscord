a80007de216bb4359bdb6e2c453d79bc2632e927
yuliia-sherstiuk <yuliia.sherstiuk@laplateforme.io>
2025-04-28 10:57:40 +0200

    Added table Channel_memberships

--- mydiscord_dump.sql
+++ mydiscord_dump.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 17.4
 -- Dumped by pg_dump version 17.4
 
--- Started on 2025-04-23 11:03:59
+-- Started on 2025-04-28 10:54:18
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -22,6 +22,20 @@ SET row_security = off;
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- TOC entry 223 (class 1259 OID 16586)
+-- Name: channel_memberships; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.channel_memberships (
+    user_id integer NOT NULL,
+    channel_id integer NOT NULL,
+    role character varying(20) DEFAULT 'member'::character varying
+);
+
+
+ALTER TABLE public.channel_memberships OWNER TO postgres;
 
 --
 -- TOC entry 220 (class 1259 OID 16542)
@@ -57,7 +71,7 @@ CREATE SEQUENCE public.channels_id_seq
 ALTER SEQUENCE public.channels_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4885 (class 0 OID 0)
+-- TOC entry 4894 (class 0 OID 0)
 -- Dependencies: 219
 -- Name: channels_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -102,7 +116,7 @@ CREATE SEQUENCE public.messages_id_seq
 ALTER SEQUENCE public.messages_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4886 (class 0 OID 0)
+-- TOC entry 4895 (class 0 OID 0)
 -- Dependencies: 221
 -- Name: messages_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -121,10 +135,9 @@ CREATE TABLE public.users (
     last_name character varying(50),
     email character varying(100) NOT NULL,
     password_hash text NOT NULL,
-    role character varying(20) DEFAULT 'member'::character varying,
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    "Question _1" text,
-    "Question_2" text
+    question_1 text,
+    question_2 text
 );
 
 
@@ -147,7 +160,7 @@ CREATE SEQUENCE public.users_id_seq
 ALTER SEQUENCE public.users_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4887 (class 0 OID 0)
+-- TOC entry 4896 (class 0 OID 0)
 -- Dependencies: 217
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -156,7 +169,7 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
--- TOC entry 4708 (class 2604 OID 16545)
+-- TOC entry 4711 (class 2604 OID 16545)
 -- Name: channels id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -164,7 +177,7 @@ ALTER TABLE ONLY public.channels ALTER COLUMN id SET DEFAULT nextval('public.cha
 
 
 --
--- TOC entry 4711 (class 2604 OID 16563)
+-- TOC entry 4714 (class 2604 OID 16563)
 -- Name: messages id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -172,7 +185,7 @@ ALTER TABLE ONLY public.messages ALTER COLUMN id SET DEFAULT nextval('public.mes
 
 
 --
--- TOC entry 4705 (class 2604 OID 16532)
+-- TOC entry 4709 (class 2604 OID 16532)
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -180,7 +193,17 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
--- TOC entry 4877 (class 0 OID 16542)
+-- TOC entry 4888 (class 0 OID 16586)
+-- Dependencies: 223
+-- Data for Name: channel_memberships; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.channel_memberships (user_id, channel_id, role) FROM stdin;
+\.
+
+
+--
+-- TOC entry 4885 (class 0 OID 16542)
 -- Dependencies: 220
 -- Data for Name: channels; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -193,7 +216,7 @@ COPY public.channels (id, name, is_private, members, owner_id, created_at) FROM 
 
 
 --
--- TOC entry 4879 (class 0 OID 16560)
+-- TOC entry 4887 (class 0 OID 16560)
 -- Dependencies: 222
 -- Data for Name: messages; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -206,26 +229,28 @@ COPY public.messages (id, user_id, channel_id, content, is_thread, parent_messag
 
 
 --
--- TOC entry 4875 (class 0 OID 16529)
+-- TOC entry 4883 (class 0 OID 16529)
 -- Dependencies: 218
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.users (id, first_name, last_name, email, password_hash, role, created_at, "Question _1", "Question_2") FROM stdin;
-1	Alice	Johnson	alice.johnson@example.com	hashed_pwd_1	admin	2025-04-18 06:29:19.836302	\N	\N
-2	Elti	Smith	elti.smith@example.com	hashed_pwd_2	moderator	2025-04-18 06:29:19.836302	\N	\N
-3	Chris	Brown	chris.brown@example.com	hashed_pwd_3	member	2025-04-18 06:29:19.836302	\N	\N
-4	Diana	Miller	diana.miller@example.com	hashed_pwd_4	member	2025-04-18 06:29:19.836302	\N	\N
-5	Fiona	Davis	fiona.davis@example.com	hashed_pwd_6	moderator	2025-04-18 08:01:36.625345	\N	\N
-6	George	Evans	george.evans@example.com	hashed_pwd_7	member	2025-04-18 08:01:36.625345	\N	\N
-7	Leila	Wilson	leila.wilson@example.com	hashed_pwd_8	admin	2025-04-18 08:01:36.625345	\N	\N
-8	Vanessa	Taylor	vanessa.taylor@example.com	hashed_pwd_9	member	2025-04-18 08:01:36.625345	\N	\N
-9	Julia	Moore	julia.moore@example.com	hashed_pwd_10	member	2025-04-18 08:01:36.625345	\N	\N
+COPY public.users (id, first_name, last_name, email, password_hash, created_at, question_1, question_2) FROM stdin;
+1	Alice	Johnson	alice.johnson@example.com	hashed_pwd_1	2025-04-18 06:29:19.836302	Shrek	Balut
+2	Elti	Smith	elti.smith@example.com	hashed_pwd_2	2025-04-18 06:29:19.836302	SpongeBob	Durian
+3	Chris	Brown	chris.brown@example.com	hashed_pwd_3	2025-04-18 06:29:19.836302	Bugs	Haggis
+4	Diana	Miller	diana.miller@example.com	hashed_pwd_4	2025-04-18 06:29:19.836302	Goofy	Escargot
+5	Fiona	Davis	fiona.davis@example.com	hashed_pwd_6	2025-04-18 08:01:36.625345	Jerry	Tripe
+7	Leila	Wilson	leila.wilson@example.com	hashed_pwd_8	2025-04-18 08:01:36.625345	Simba	Natto
+8	Vanessa	Taylor	vanessa.taylor@example.com	hashed_pwd_9	2025-04-18 08:01:36.625345	Pikachu	Tofu
+9	Julia	Moore	julia.moore@example.com	hashed_pwd_10	2025-04-18 08:01:36.625345	Tom	Insects
+6	George	Evans	george.evans@example.com	hashed_pwd_7	2025-04-18 08:01:36.625345	Naruto	grenouille
+10	Yuliia	Sherstiuk	yuliia.sherstiuk@laplateforme.io	zyx~}	2025-04-25 15:23:13.431333	Balto	Escargot
+11	eltigani	Abdallah	eltigani.abdallah@laplateforme.io	)..;)$$;	2025-04-28 09:47:04.86208	mama bear	milkshake mangue
 \.
 
 
 --
--- TOC entry 4888 (class 0 OID 0)
+-- TOC entry 4897 (class 0 OID 0)
 -- Dependencies: 219
 -- Name: channels_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -234,7 +259,7 @@ SELECT pg_catalog.setval('public.channels_id_seq', 3, true);
 
 
 --
--- TOC entry 4889 (class 0 OID 0)
+-- TOC entry 4898 (class 0 OID 0)
 -- Dependencies: 221
 -- Name: messages_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -243,16 +268,25 @@ SELECT pg_catalog.setval('public.messages_id_seq', 3, true);
 
 
 --
--- TOC entry 4890 (class 0 OID 0)
+-- TOC entry 4899 (class 0 OID 0)
 -- Dependencies: 217
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 9, true);
+SELECT pg_catalog.setval('public.users_id_seq', 11, true);
 
 
 --
--- TOC entry 4720 (class 2606 OID 16553)
+-- TOC entry 4730 (class 2606 OID 16591)
+-- Name: channel_memberships channel_memberships_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.channel_memberships
+    ADD CONSTRAINT channel_memberships_pkey PRIMARY KEY (user_id, channel_id);
+
+
+--
+-- TOC entry 4724 (class 2606 OID 16553)
 -- Name: channels channels_name_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -261,7 +295,7 @@ ALTER TABLE ONLY public.channels
 
 
 --
--- TOC entry 4722 (class 2606 OID 16551)
+-- TOC entry 4726 (class 2606 OID 16551)
 -- Name: channels channels_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -270,7 +304,7 @@ ALTER TABLE ONLY public.channels
 
 
 --
--- TOC entry 4724 (class 2606 OID 16570)
+-- TOC entry 4728 (class 2606 OID 16570)
 -- Name: messages messages_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -279,7 +313,7 @@ ALTER TABLE ONLY public.messages
 
 
 --
--- TOC entry 4716 (class 2606 OID 16540)
+-- TOC entry 4720 (class 2606 OID 16540)
 -- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -288,7 +322,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 4718 (class 2606 OID 16538)
+-- TOC entry 4722 (class 2606 OID 16538)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -297,7 +331,25 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 4725 (class 2606 OID 16554)
+-- TOC entry 4735 (class 2606 OID 16597)
+-- Name: channel_memberships channel_memberships_channel_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.channel_memberships
+    ADD CONSTRAINT channel_memberships_channel_id_fkey FOREIGN KEY (channel_id) REFERENCES public.channels(id) ON DELETE CASCADE;
+
+
+--
+-- TOC entry 4736 (class 2606 OID 16592)
+-- Name: channel_memberships channel_memberships_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.channel_memberships
+    ADD CONSTRAINT channel_memberships_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+
+
+--
+-- TOC entry 4731 (class 2606 OID 16554)
 -- Name: channels channels_owner_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -306,7 +358,7 @@ ALTER TABLE ONLY public.channels
 
 
 --
--- TOC entry 4726 (class 2606 OID 16576)
+-- TOC entry 4732 (class 2606 OID 16576)
 -- Name: messages messages_channel_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -315,7 +367,7 @@ ALTER TABLE ONLY public.messages
 
 
 --
--- TOC entry 4727 (class 2606 OID 16581)
+-- TOC entry 4733 (class 2606 OID 16581)
 -- Name: messages messages_parent_message_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -324,7 +376,7 @@ ALTER TABLE ONLY public.messages
 
 
 --
--- TOC entry 4728 (class 2606 OID 16571)
+-- TOC entry 4734 (class 2606 OID 16571)
 -- Name: messages messages_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -332,7 +384,7 @@ ALTER TABLE ONLY public.messages
     ADD CONSTRAINT messages_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE SET NULL;
 
 
--- Completed on 2025-04-23 11:03:59
+-- Completed on 2025-04-28 10:54:19
 
 --
 -- PostgreSQL database dump complete
