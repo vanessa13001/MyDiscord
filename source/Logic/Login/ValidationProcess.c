674d416b9ca68727689d638eea01a56650b86009
christine chemali <christine.chemali@laplateforme.io>
2025-04-29 13:37:15 +0200

    add validation process file to verify if email and password meet the requirements

---
+++ source/Logic/Login/ValidationProcess.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <libpq-fe.h>
+#include "ValidationProcess.h"
+
+// Validate email process
+bool validate_email(const char *email) {
+    return strstr(email, "@") != NULL && strstr(email, ".") != NULL;
+}
+
+// Validate password process
+bool validate_password(const char *password) {
+    if (strlen(password) < 10) return false;
+    bool has_upper = false, has_lower = false, has_digit = false, has_special = false;
+    for (int i = 0; i < strlen(password); i++) {
+        if (isupper(password[i])) has_upper = true;
+        if (islower(password[i])) has_lower = true;
+        if (isdigit(password[i])) has_digit = true;
+        if (strchr("?,.;/:§!%&*()-+|@", password[i])) has_special = true;
+    }
+    return has_upper && has_lower && has_digit && has_special;
+}
