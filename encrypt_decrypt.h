a516c5279001c612763c2f9db6ea3e488ed2393e
vanessa13001 <vanessa.sabatier@laplateforme.io>
2025-04-30 07:18:24 +0200

    feat: fix encryption branch

---
+++ encrypt_decrypt.h
@@ -0,0 +1,7 @@
+#ifndef ENCRYPT_DECRYPT_H
+#define ENCRYPT_DECRYPT_H
+
+void xor_cipher(char *message, const char *key);
+void generate_random_key(char *key, int length);
+
+#endif // ENCRYPT_DECRYPT_H
